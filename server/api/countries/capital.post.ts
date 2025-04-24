670fb2284edac3b0cbf0ec1896a51d4f5632113f
prospermbuma <prospermbuma@gmail.com>
2025-04-24 10:30:24 +0300

    Updates on capital

--- server/api/countries/capital.post.ts
+++ server/api/countries/capital.post.ts
@@ -1,48 +1,26 @@
 /* ========================================================
 # Method: Post
-# Endpoint: api/countries/by-capital
+# Endpoint: api/countries/capital
 # Body: {capital: string}
 =========================================================*/
 
-// import { countries, countryList } from "@doubco/countries";
-// import countries from "@doubco/countries";
-
-// console.log(countries);
-
-// export default defineEventHandler(async (event) => {
-//   const body = await readBody(event);
-//   const { capital } = body;
-//   return {
-
-//   };
-// });
-
-// import countries from "@doubco/countries";
-// console.log(countries);
-
-// export default defineEventHandler(async (event) => {
-//   const body = await readBody(event);
-//   const { capital } = body;
-
-//   // Find the country by capital (case-insensitive match)
-//   const country = Object.values(countries).find(
-//     (c) => c.capital.toLowerCase() === capital.toLowerCase()
-//   );
-
-//   return {
-//     success: !!country,
-//     data: country || null,
-//     message: country ? "Country found" : "Country not found"
-//   };
-// });
-
-import countries from "@doubco/countries";
+import { countries } from "@doubco/countries";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { capital } = body;
+  if (!capital) {
+    return {
+      status: 400,
+      message: "No capital provided in body",
+    };
+  }
   const countriesArr = Object.values(countries);
   return {
-    countries: countriesArr.filter((Object) => Object.data.includes(capital)),
+    countries: countriesArr.filter((Object) => {
+      if (Object.capital) {
+        return Object.capital.includes(capital);
+      }
+    }),
   };
 });
