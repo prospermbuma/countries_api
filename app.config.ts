f3fc0a64925a362e36af54e591b4e4238f8e473c
prospermbuma <prospermbuma@gmail.com>
2025-04-24 07:45:16 +0300

    Added more content's routes for APIs documentation

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'FakeCountries API',
+    title: 'Countries API',
     description: 'API to fetch any country in the world with its name, its id code and more information.',
     image: '',
     socials: {
